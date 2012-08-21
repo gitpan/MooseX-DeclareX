e320760cb50c6a48592c6fb16b7dc2086ef43148
Toby Inkster <tobyink@cpan.org>
2012-08-21 12:21:15 +0000

    Import of TOBYINK/MooseX-DeclareX-0.001 from CPAN.
    gitpan-cpan-distribution: MooseX-DeclareX
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         TOBYINK/MooseX-DeclareX-0.001.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

---
+++ lib/MooseX/DeclareX/Keyword/namespace.pm
@@ -0,0 +1,19 @@
+package MooseX::DeclareX::Keyword::namespace;
+
+BEGIN {
+	$MooseX::DeclareX::Keyword::namespace::AUTHORITY = 'cpan:TOBYINK';
+	$MooseX::DeclareX::Keyword::namespace::VERSION   = '0.001';
+}
+
+require MooseX::Declare;
+
+use Moose;
+extends 'MooseX::Declare::Syntax::Keyword::Namespace';
+with 'MooseX::DeclareX::Plugin';
+with 'MooseX::DeclareX::Registry';
+
+sub preferred_identifier { 'namespace' }
+
+sub add_optional_customizations {}
+
+1;
---
+++ lib/MooseX/DeclareX/Keyword/role.pm
@@ -0,0 +1,23 @@
+package MooseX::DeclareX::Keyword::role;
+
+BEGIN {
+	$MooseX::DeclareX::Keyword::role::AUTHORITY = 'cpan:TOBYINK';
+	$MooseX::DeclareX::Keyword::role::VERSION   = '0.001';
+}
+
+require MooseX::Declare;
+
+use Moose;
+extends 'MooseX::Declare::Syntax::Keyword::Role';
+with 'MooseX::DeclareX::Plugin';
+with 'MooseX::DeclareX::Registry';
+
+sub preferred_identifier { 'role' }
+
+before add_namespace_customizations => sub {
+	my ($self, $ctx) = @_;
+	$_->setup_for($ctx->namespace, provided_by => ref $self)
+		foreach @{ $self->default_inner };
+};
+
+1;
